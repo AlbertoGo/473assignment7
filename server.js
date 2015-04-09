60b413de48e563e867f6f442a9bc74dd40a58aa8
Alberto Gomez <Otrebla117@csu.fullerton.edu>
2015-04-08 18:56:26 -0700

    Added body-parser

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var express = require("express"),
 	http = require("http"),
+	bodyParser = require("body-parser"),
+	redis = require("redis"),
 	app;
+
+var client = redis.createClient();
 
 app = express();
 
 app.use(express.static(__dirname + "/html"));
+app.use(bodyParser());
 
 http.createServer(app).listen(3000);
 
-app.get("/hello", function (req, res){
-	res.send("Hello World!");
-});
+//app.get("/test", function (req, res){
+//	res.redirect('test.html');
+//});
 
-app.get("/goodbye", function (req, res){
-	res.send("Goodbye, World!");
+app.post("/", function (req, res){
+	var test = req.body.URL;
+	console.log(test);
 });
 
 console.log("listening on 3000")
